fd846a5c3d28b2be6e479278b70059bc95f8f419
Blashadow <blackzerogamer@gmail.com>
2015-08-03 19:51:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+/** Dependency **/
+var socketIO = require('socket.io');
+var debug = require('debug')('remote-logger');
+
+module.exports = function(app){
+
+    /** Construct **/
+    var io = socketIO(app);
+    
+    io.on('connection',function(socket){
+        log('New client connected');
+    });
+    
+    io.on('disconnect',function(){
+       log('Client left'); 
+    });
+    
+    /*
+     * Send those event logs  
+     * @param data - Option Object
+     *
+     * **/
+    var emitLog = function(data){
+        
+        io.emit("log", data);
+        
+        io.emit("log".data.log_type, data);
+    };
+     
+    /*
+     * Log events using debug and web sockets.
+     *
+     * **/
+    var log = function(data){
+        debug(data.message);
+        
+        emitLog(data);
+    };
+    
+    /* 
+     * Option log builder
+     *
+     * **/
+    var optionsBuilder = function(message, type, extraArguments){
+        return {
+            message: message,
+            log_type: type,
+            extra: extraArguments
+        };
+    };
+    
+    /** Public methods **/
+    return {
+        info: function(format, params){
+            log(optionsBuilder(format, "INFO", params));    
+        },
+        debug: function(format, params){
+            log(optionsBuilder(format, "INFO", params));
+        },
+        error: function(format, params){
+            log(optionsBuilder(format, "INFO", params));
+        },
+        fatal: function(format, params){
+            log(optionsBuilder(format, "FATAL", params));
+        }
+    };
+};
+
+
